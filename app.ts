6e5dd280c815b27fc2df19a0f220ec1b921cd1c9
BernardSapida <BernardSapida1706@gmail.com>
2023-02-21 14:17:06 +0800

    update

--- app.ts
+++ app.ts
@@ -4,16 +4,16 @@ import route from "./routes/authentication.ts";
 
 const app = new Application();
 
-Deno.env.set("APP_PASSWORD", "gcesgedkqhxmdakv");
-Deno.env.set(
-  "DATA_API_KEY",
-  "UTO769Anhub6OIq2NXaOhEr4ce1MuiAtcdPOIkDTmcdwlCm3CWQ7jM34Lj8epHG9",
-);
-Deno.env.set("APP_ID", "data-phvhu");
-Deno.env.set("DATA_SOURCE", "ZShop");
-Deno.env.set("DB_CLUSTER_URL", "zshop.k1sczh5.mongodb.net");
-Deno.env.set("DATABASE", "shop");
-Deno.env.set("DATABASE_PASSWORD", "ZShop123");
+// Deno.env.set("APP_PASSWORD", "gcesgedkqhxmdakv");
+// Deno.env.set(
+//   "DATA_API_KEY",
+//   "UTO769Anhub6OIq2NXaOhEr4ce1MuiAtcdPOIkDTmcdwlCm3CWQ7jM34Lj8epHG9",
+// );
+// Deno.env.set("APP_ID", "data-phvhu");
+// Deno.env.set("DATA_SOURCE", "ZShop");
+// Deno.env.set("DB_CLUSTER_URL", "zshop.k1sczh5.mongodb.net");
+// Deno.env.set("DATABASE", "shop");
+// Deno.env.set("DATABASE_PASSWORD", "ZShop123");
 
 app.use(oakCors(
   {
